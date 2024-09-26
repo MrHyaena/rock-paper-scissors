13c14ba0ae5ffa277ce9e612ea22af6e8b3f16a4
MrHyaena <dolezalmartin131@gmail.com>
2024-09-26 19:33:29 +0200

    Vytvořil jsem funkci pro náhodnou volbu počítače

--- javascript.js
+++ javascript.js
@@ -8,4 +8,24 @@ let c = "scissors"
 let pc = ""
 let human = "" 
 
+// This is the function for making a random choice for computer
+
+function getComputerChoice() {
+
+    let number = Math.random()
+
+    console.log(number)
+
+    if (number < 1 / 3) {
+    pc = a
+    } else if (number < 2 / 3) {
+    pc = b
+    } else {
+    pc = c
+    }
+
+   return pc
+}
+
+
 
